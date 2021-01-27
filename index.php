4419ac96171597533ee9826b658b1eec659d5edd
Clems-web <clement.servais1@gmail.com>
2021-01-27 10:44:54 +0100

    Terminé !

--- index.php
+++ index.php
@@ -5,15 +5,16 @@
  */
 
 // TODO Votre code ici.
-
-
+$result = rand(0,50);
+echo "<div> $result </div>";
 
 /**
  * 2. Générez un nombre aléatoire compris entre 50 et getrandmax, affichez le résultat dans un nouveau <p>
  */
 
 // TODO Votre code ici.
-
+$result = rand(50, getrandmax());
+echo "<p> $result </p>";
 
 /**
  * 3. Générez un nombre aléatoire compris entre 0 et 50
@@ -23,6 +24,13 @@
 
 // TODO Votre code ici.
 
+$result = rand(0, 100);
+if ($result <= 50) {
+    echo "<p> gagné </p>";
+}
+else {
+    echo "perdu <br>";
+}
 
 /**
  * 4. Créez une fonction qui génère un nombre aléatoire compris entre $max et $min ( paramètres ).
@@ -33,8 +41,16 @@
 // TODO Votre code ici.
 
 
+function random($minimum, $maxi) {
+    $result = rand($minimum, $maxi);
+    if ($result <= $maxi && $result >= ($maxi - 100)) {
+        random($minimum,$maxi);
+    }
+    else {
+        echo $result;
+    }
+}
 
 
-
-
+random(5,1000);
 
